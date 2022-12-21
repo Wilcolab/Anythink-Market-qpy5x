bedfad26b1d5992cb41da4bc78f4d73c0f57901f
hahahoho <hahahoho@hahahohoui-MacBookAir.local>
2022-12-21 13:06:23 +0900

    wip

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,9 +1,11 @@
+
+rm $2
 while IFS="," read -r id last_name first_name email price country
 do
   if [[ $email == *"@amazon"* ]]; then
-    echo "$first_name $last_name"
+    echo "$first_name $last_name" >> $2
   fi
   if [[ $email == *"@Amazon"* ]]; then
-    echo "$first_name $last_name"
+    echo "$first_name $last_name" >> $2 
   fi
-done
+done < $1
